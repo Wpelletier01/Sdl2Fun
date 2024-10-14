51bca5b01610631196aab25c88f51f64fe3b1f40
will <1834089@crosemont.qc.ca>
2024-10-14 12:08:38 -0400

    dev texture loader

--- include/asset_manager.h
+++ include/asset_manager.h
@@ -2,12 +2,25 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include "core/texture.h"
+#include "declaration.h"
+#include <map>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 
 class AssetManager {
 
     public:
+
         int init(SDL_Renderer* renderer);
+
+
+    private:
+
+        std::map<std::string,Texture> textures; 
 
 
 };
--- src/asset_manager.cpp
+++ src/asset_manager.cpp
@@ -3,6 +3,26 @@
 
 int AssetManager::init(SDL_Renderer* renderer) {
 
+    // Load texture 
+
+    fs::path p;
+
+    for (const auto &entry : fs::directory_iterator(ASSET_IMG_DIR)) {
+
+        p = entry.path();
+
+        Texture tex(p,renderer);
+
+        this->textures.insert({(std::string)p.filename(),tex});
+
+        SDL_Log("load texture: '%s'",entry.path().c_str());
+    }
+
+
+
+    //    
+
+
     return 0;
 }
 
