b684c01c72c2acaf28f963f234feb2003b1fd37c
will <1834089@crosemont.qc.ca>
2024-10-14 11:35:06 -0400

    clean

--- include/asset_manager.h
+++ include/asset_manager.h
@@ -9,8 +9,5 @@ class AssetManager {
     public:
         int init(SDL_Renderer* renderer);
 
-    private:
-        SDL_Texture* loadTexture(const char* img, SDL_Renderer* renderer);
 
-        SDL_Texture* playerSprite = NULL;
 };
--- src/asset_manager.cpp
+++ src/asset_manager.cpp
@@ -3,32 +3,6 @@
 
 int AssetManager::init(SDL_Renderer* renderer) {
 
-    int imgFlag = IMG_INIT_PNG | IMG_INIT_JPG;
-
-    if (IMG_Init(imgFlag) == 0) {
-        SDL_LogError(1,"Couldn't initialise image module");
-        return 1;
-    }
-
-    this->playerSprite = loadingTexture("assets/image/player.png");
-
     return 0;
 }
 
-
-SDL_Texture* AssetManager::loadTexture(const char* img, SDL_Renderer* renderer) {
-
-    SDL_Texture* loadingTexture = NULL;
-
-    SDL_Surface* loadSurface = IMG_Load(img);
-
-    if (loadSurface == NULL) {
-        printf("Couldn't load img '%s'. Error: %s",img,SDL_GetError());
-        return NULL;
-    }
-
-    loadingTexture = SDL_CreateTextureFromSurface(renderer, loadSurface);
-    SDL_FreeSurface(loadSurface);
-
-    return loadingTexture;
-}
