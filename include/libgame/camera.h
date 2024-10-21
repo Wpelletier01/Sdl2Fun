49776b844b9a65b27f6e2dfca8a92ef180812306
william <wpelletier.development@yahoo.com>
2024-10-21 13:04:46 -0400

    add camera class

---
+++ include/libgame/camera.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class Camera
+{
+    public:
+        Camera(int sx, int sy, int sw, int sh);
+
+    private:
+
+        int x,y,w,h; 
+
+
+};
---
+++ src/libgame/camera.cpp
@@ -0,0 +1,11 @@
+#include "camera.h"
+
+
+Camera::Camera(int sx, int sy, int sw, int sh)
+{
+    this->x = sx;
+    this->y = sy;
+    this->w = sw;
+    this->h = sh;
+    
+}
